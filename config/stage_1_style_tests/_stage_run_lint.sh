a8926fb72cc6410b19e962b002eaab2e9d6ac13f
Andrei K <logerk3@gmail.com>
2022-09-08 22:46:25 +0300

    use exit-zero

--- config/stage_1_style_tests/_stage_run_lint.sh
+++ config/stage_1_style_tests/_stage_run_lint.sh
@@ -18,9 +18,7 @@ for LAB_NAME in $LABS; do
 	echo "Running lint for lab ${LAB_NAME}"
   TARGET_SCORE=$(bash config/get_mark.sh ${LAB_NAME})
 
-	lint_output=$(python -m pylint --rcfile config/stage_1_style_tests/.pylintrc ${LAB_NAME})
-
-	echo "$lint_output"
+	lint_output=$(python -m pylint --exit-zero --rcfile config/stage_1_style_tests/.pylintrc ${LAB_NAME})
 
   python config/stage_1_style_tests/lint_level.py \
           --lint-output "${lint_output}" \
