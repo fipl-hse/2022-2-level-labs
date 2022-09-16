dfddc96ab5d17a56b717e66b779aa4dd0058a764
Tasenko03 <93547677+Tasenko03@users.noreply.github.com>
2022-09-16 03:22:50 +0300

    fix get_top_n

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -3,33 +3,42 @@ Frequency-driven keyword extraction starter
 """
 import json
 from pathlib import Path
-from main import (correct_type, clean_and_tokenize, remove_stop_words, calculate_frequencies, get_top_n, calculate_tf,
-                  calculate_tfidf, calculate_expected_frequency, calculate_chi_values, extract_significant_words)
+from main import (
+    clean_and_tokenize,
+    remove_stop_words,
+    calculate_frequencies,
+    get_top_n,
+    calculate_tf,
+    calculate_tfidf,
+    calculate_expected_frequency,
+    calculate_chi_values,
+    extract_significant_words,
+)
 
 if __name__ == "__main__":
 
     # finding paths to the necessary utils
     PROJECT_ROOT = Path(__file__).parent
-    ASSETS_PATH = PROJECT_ROOT / 'assets'
+    ASSETS_PATH = PROJECT_ROOT / "assets"
 
     # reading the text from which keywords are going to be extracted
-    TARGET_TEXT_PATH = ASSETS_PATH / 'Дюймовочка.txt'
-    with open(TARGET_TEXT_PATH, 'r', encoding='utf-8') as file:
+    TARGET_TEXT_PATH = ASSETS_PATH / "Дюймовочка.txt"
+    with open(TARGET_TEXT_PATH, "r", encoding="utf-8") as file:
         target_text = file.read()
 
     # reading list of stop words
-    STOP_WORDS_PATH = ASSETS_PATH / 'stop_words.txt'
-    with open(STOP_WORDS_PATH, 'r', encoding='utf-8') as file:
-        stop_words = file.read().split('\n')
+    STOP_WORDS_PATH = ASSETS_PATH / "stop_words.txt"
+    with open(STOP_WORDS_PATH, "r", encoding="utf-8") as file:
+        stop_words = file.read().split("\n")
 
     # reading IDF scores for all tokens in the corpus of H.C. Andersen tales
-    IDF_PATH = ASSETS_PATH / 'IDF.json'
-    with open(IDF_PATH, 'r', encoding='utf-8') as file:
+    IDF_PATH = ASSETS_PATH / "IDF.json"
+    with open(IDF_PATH, "r", encoding="utf-8") as file:
         idf = json.load(file)
 
     # reading frequencies for all tokens in the corpus of H.C. Andersen tales
-    CORPUS_FREQ_PATH = ASSETS_PATH / 'corpus_frequencies.json'
-    with open(CORPUS_FREQ_PATH, 'r', encoding='utf-8') as file:
+    CORPUS_FREQ_PATH = ASSETS_PATH / "corpus_frequencies.json"
+    with open(CORPUS_FREQ_PATH, "r", encoding="utf-8") as file:
         corpus_freqs = json.load(file)
 
 split_words = clean_and_tokenize(target_text)
@@ -46,6 +55,6 @@ top_x2 = get_top_n(x2_dict, 10)
 print(top_x2)
 significant_words_dict = extract_significant_words(x2_dict, 0.05)
 
-RESULT = None
-    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Keywords are not extracted'
+RESULT = top_x2
+# DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+assert RESULT, "Keywords are not extracted"
