60d29bc7f4d2b52a322c77112917454bd30ae130
AndreyKarnauhov <91670823+AndreyKarnauhov@users.noreply.github.com>
2022-09-17 18:38:18 +0300

    removed list output from clean_and_tokenize; improved the function, so it should pass all the tests

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -20,11 +20,10 @@ def clean_and_tokenize(text: str) -> Optional[list[str]]:
     if not isinstance(text, str):
         return None
     text = text.lower()
-    for i in '.,:-!':
+    for i in '.,:-!;%<>&*@#':
         # "что-то" и "маленькая-маленькая" обрабатываются как одно слово. Не стоит ли заменять пробелами?
         text = text.replace(i, '')
     text = text.split()
-    print(text)
     return text
 
 
