93ad2ab78ca1608bb84d8333960e09ef6ba7ff47
Chris <kristinanigamedzanova@gmail.com>
2022-09-19 01:04:44 +0300

    made five functions

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -5,51 +5,45 @@ Extract keywords based on frequency related metrics
 from typing import Optional, Union
 
 
-def clean_and_tokenize(text: str) -> Optional[list[str]]:
-
-    """
-    Removes punctuation, casts to lowercase, splits into tokens
-
-    Parameters:
-    text (str): Original text
-
-    Returns:
-    list[str]: A sequence of lowercase tokens with no punctuation
-
-    In case of corrupt input arguments, None is returned
-    """
-    pass
-
+def clean_and_tokenize(text: str):
+    if isinstance(text, str) is False:
+        return None
+    else:
+        text = text.lower()
+        text = text.strip()
+        marks = ",.:;!?—(){}[]&"
+        # добавить удаление  пробелов рядом с пробелами
+        for i in text:
+            if i in marks:
+                text = text.replace(i, "")
+            else:
+                pass
+        list = text.split()
+        print(list)
 
 def remove_stop_words(tokens: list[str], stop_words: list[str]) -> Optional[list[str]]:
-    """
-    Excludes stop words from the token sequence
-
-    Parameters:
-    tokens (List[str]): Original token sequence
-    stop_words (List[str]: Tokens to exclude
-
-    Returns:
-    List[str]: Token sequence that does not include stop words
-
-    In case of corrupt input arguments, None is returned
-    """
-    pass
+    if isinstance(tokens, list) is False or isinstance(stop_words, list) is False:
+        return None
+    elif len(tokens) == 0 or len(stop_words) == 0:
+        return None
+    for word in stop_words:
+        while word in tokens:
+            tokens.remove(word)   #апдейт кажется я починила но все равно нужно чекнуть (код пропускал некоторые слова)
+    print(tokens)
 
 
 def calculate_frequencies(tokens: list[str]) -> Optional[dict[str, int]]:
-    """
-    Composes a frequency dictionary from the token sequence
-
-    Parameters:
-    tokens (List[str]): Token sequence to count frequencies for
-
-    Returns:
-    Dict: {token: number of occurrences in the token sequence} dictionary
-
-    In case of corrupt input arguments, None is returned
-    """
-    pass
+    frequencies = {}
+    if isinstance(tokens, list) is False or len(tokens) == 0:
+        return None
+    for i in tokens:
+        if isinstance(i, str) is False:
+            return None
+    else:
+        for i in tokens:
+            x = tokens.count(i)
+            frequencies[i] = x
+    print(frequencies)
 
 
 def get_top_n(frequencies: dict[str, Union[int, float]], top: int) -> Optional[list[str]]:
@@ -67,7 +61,19 @@ def get_top_n(frequencies: dict[str, Union[int, float]], top: int) -> Optional[l
 
     In case of corrupt input arguments, None is returned
     """
-    pass
+    if isinstance(frequencies, dict) is False or (isinstance(top, (int, float)) is False ):
+        return None
+    else:
+        freq_val = list(frequencies.values())
+        words_num = 0
+        for value in freq_val:
+            words_num += value
+        print(words_num)
+        if top <= words_num:
+            top_list = sorted(frequencies, key = frequencies.get, reverse = True)[:top]  #get возвращает значение по указанному ключу
+        else:
+            top_list = sorted(frequencies, key = frequencies.get, reverse = True)
+        print(top_list)
 
 
 def calculate_tf(frequencies: dict[str, int]) -> Optional[dict[str, float]]:
@@ -83,7 +89,18 @@ def calculate_tf(frequencies: dict[str, int]) -> Optional[dict[str, float]]:
 
     In case of corrupt input arguments, None is returned
     """
-    pass
+    if isinstance(frequencies, dict) is False:   #добавить проверку на то, являются ли ключи строками а значения интами
+        return None
+    else:
+        freq_values = list(frequencies.values())
+        words_num = 0
+        for value in freq_values:
+            words_num += value
+        tf_dict = {}
+        for key, val in frequencies.items():
+            tf = val / words_num
+            tf_dict[key] = round(tf, 3)   #нужен ли тут раунд?
+        print(tf_dict)
 
 
 def calculate_tfidf(term_freq: dict[str, float], idf: dict[str, float]) -> Optional[dict[str, float]]:
@@ -100,7 +117,7 @@ def calculate_tfidf(term_freq: dict[str, float], idf: dict[str, float]) -> Optio
 
     In case of corrupt input arguments, None is returned
     """
-    pass
+
 
 
 def calculate_expected_frequency(
