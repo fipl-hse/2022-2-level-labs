e8d865b58aa3ba47ab263b3ba38543a008488639
Alexandra Eliseeva <klen.sasha_22@list.ru>
2022-09-19 22:33:13 +0300

    code style correction

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -3,7 +3,6 @@ Frequency-driven keyword extraction starter
 """
 import json
 from pathlib import Path
-
 from typing import Optional, Union
 
 from lab_1_keywords_tfidf.main import (clean_and_tokenize, remove_stop_words,
