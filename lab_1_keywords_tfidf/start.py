c45304eaa675d915f9dd1305fc0bf31575429b84
YuliaVodopyanova <vodopyanovaye@mail.ru>
2022-09-20 14:59:26 +0300

    some improvements

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -9,7 +9,7 @@ import main
 if __name__ == "__main__":
 
     # finding paths to the necessary utils
-    PROJECT_ROOT = Path('new').parent
+    PROJECT_ROOT = Path(__file__).parent
     ASSETS_PATH = PROJECT_ROOT / 'assets'
 
     # reading the text from which keywords are going to be extracted
@@ -32,25 +32,28 @@ if __name__ == "__main__":
     with open(CORPUS_FREQ_PATH, 'r', encoding='utf-8') as file:
         corpus_freqs = json.load(file)
 
-    print(main.clean_and_tokenize(target_text))
+    # main.clean_and_tokenize(target_text)
     tokens = main.clean_and_tokenize(target_text)
-    print(main.remove_stop_words(tokens, stop_words))
+    # main.remove_stop_words(tokens, stop_words)
     tokens = main.remove_stop_words(tokens, stop_words)
-    print(main.calculate_frequencies(tokens))
+    # main.calculate_frequencies(tokens)
     frequencies = main.calculate_frequencies(tokens)
-    print(main.get_top_n(frequencies, 8))
-    print(main.calculate_tf(frequencies))
+    main.get_top_n(frequencies, 8)
+    # main.calculate_tf(frequencies)
     term_freq = main.calculate_tf(frequencies)
-    print(main.calculate_tfidf(term_freq, idf))
+    # main.calculate_tfidf(term_freq, idf)
     tfidf_dic = main.calculate_tfidf(term_freq, idf)
     print(main.get_top_n(tfidf_dic, 10))
-    print(main.calculate_expected_frequency(frequencies, corpus_freqs))
+    # main.calculate_expected_frequency(frequencies, corpus_freqs)
     expected_freq = main.calculate_expected_frequency(frequencies, corpus_freqs)
-    print(main.calculate_chi_values(expected_freq, frequencies))
+    # main.calculate_chi_values(expected_freq, frequencies)
+    chi_values = main.calculate_chi_values(expected_freq, frequencies)
+    main.extract_significant_words(chi_values, 0.05)
+    key_words = main.get_top_n(chi_values, 10)
+    print(key_words)
 
-
-    # RESULT = None
+    RESULT = key_words
     # # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    # assert RESULT, 'Keywords are not extracted'
+    assert RESULT, 'Keywords are not extracted'
 
 
