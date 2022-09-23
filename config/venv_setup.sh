cfb9d69282ca802d15a2a798b75ce09c717bcd2c
Andrei K <logerk3@gmail.com>
2022-09-23 11:35:27 +0300

    setup deps

--- config/venv_setup.sh
+++ config/venv_setup.sh
@@ -6,12 +6,16 @@ which python
 
 if [ -d "venv" ]; then
   echo "VENV already exists."
+  venv/bin/python -m pip install -r requirements.txt
+  venv/bin/python -m pip install -r requirements_qa.txt
   exit 0
 fi
 
 python -m pip install --upgrade pip
 python -m pip install virtualenv
 python -m virtualenv venv
+
+sleep 10
 
 which python
 
