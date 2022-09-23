cac469104d0ab5a48c69367bf5bc6924f924ec90
Alexander Demidovskij <monadv@yandex.ru>
2022-09-23 16:47:11 +0300

    back to original script

--- config/venv_setup.sh
+++ config/venv_setup.sh
@@ -4,23 +4,10 @@ set -ex
 
 which python
 
-if [ -f "venv/bin/python" ]; then
-  echo "VENV and Python already exist."
-  venv/bin/python -m pip install -r requirements.txt
-  venv/bin/python -m pip install -r requirements_qa.txt
-  exit 0
-fi
-
-if [ -d "venv" ]; then
-  echo "VENV exists but not Python exec. Setup a new venv."
-  rm -rf venv
-fi
-
 python -m pip install --upgrade pip
 python -m pip install virtualenv
 python -m virtualenv venv
 
-sleep 10
 
 source venv/bin/activate
 
