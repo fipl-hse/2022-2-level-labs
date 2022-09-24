2a22a9f123c76e66d56696959f04379fa8bc3fe6
Timur20033 <timurorigami@mail.ru>
2022-09-24 16:32:32 +0300

    ...

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -37,8 +37,7 @@ if __name__ == "__main__":
     words_with_frequencies = main.calculate_frequencies(text_without_stop_words)
     words_with_tf = main.calculate_tf(words_with_frequencies)
     words_with_tfidf = main.calculate_tfidf(words_with_tf, idf)
-    top_tfidf = main.get_top_n(words_with_tfidf)
-
+    top_tfidf = main.get_top_n(words_with_tfidf, 10)
     expected_frequency = main.calculate_expected_frequency(words_with_frequencies)
     chi = main.calculate_chi_values(expected_frequency, words_with_frequencies)
     significant_words = main.extract_significant_words(chi, 0.05)
