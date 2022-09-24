eaa8338385642fb2f554b462cee4ec936703be8a
Timur20033 <timurorigami@mail.ru>
2022-09-24 16:44:23 +0300

    message

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -35,9 +35,7 @@ if __name__ == "__main__":
     text_without_punctuation = main.clean_and_tokenize(target_text)
     text_without_stop_words = main.remove_stop_words(text_without_punctuation, stop_words)
     words_with_frequencies = main.calculate_frequencies(text_without_stop_words)
-    words_with_tf = main.calculate_tf(words_with_frequencies)
-    words_with_tfidf = main.calculate_tfidf(words_with_tf, idf)
-    top_tfidf = main.get_top_n(words_with_tfidf, 10)
+
     expected_frequency = main.calculate_expected_frequency(words_with_frequencies, corpus_freqs)
     chi = main.calculate_chi_values(expected_frequency, words_with_frequencies)
     significant_words = main.extract_significant_words(chi, 0.05)
