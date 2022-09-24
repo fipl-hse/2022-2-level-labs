3d66ae1ac39e9430fae79b55ec3382b1072241ec
Alexander Demidovskij <monadv@yandex.ru>
2022-09-24 18:45:31 +0300

    attempt no. 17

--- config/collect_coverage/run_coverage.sh
+++ config/collect_coverage/run_coverage.sh
@@ -6,12 +6,4 @@ source venv/bin/activate
 
 export PYTHONPATH="$(pwd):${PYTHONPATH}"
 
-echo "PYTHONPATH: $PYTHONPATH"
-echo "PATH $PATH"
-echo "WHICH PYTHON: "
-which python
-
-echo "LS -LA venv/bin"
-ls -la venv/bin
-
 python config/collect_coverage/coverage_analyzer.py
--- config/stage_1_style_tests/_stage_pr_name_check.sh
+++ config/stage_1_style_tests/_stage_pr_name_check.sh
@@ -6,12 +6,4 @@ echo "Pull Request Name is $1"
 
 source venv/bin/activate
 
-echo "PYTHONPATH: $PYTHONPATH"
-echo "PATH $PATH"
-echo "WHICH PYTHON: "
-which python
-
-echo "LS -LA venv/bin"
-ls -la venv/bin
-
 python config/stage_1_style_tests/pr_name_check.py --pr-name="$1" --pr-author="$2"
