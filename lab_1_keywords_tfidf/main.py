4d680032fc060a469f05fe930cfab6a2c5f51e8f
Timur20033 <timurorigami@mail.ru>
2022-09-25 16:47:52 +0300

    names fixed

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -78,20 +78,11 @@ def calculate_frequencies(tokens: list[str]) -> Optional[dict[str, int]]:
     In case of corrupt input arguments, None is returned
     """
     if isinstance(tokens, list) and tokens != []:
-        for a in tokens:
-            if isinstance(a, str):
-                for j in tokens:
-                    if type(j) == str:
-                        frequency_dictionary = {}
-                        for i in tokens:
-                            frequency_dictionary.update({i: tokens.count(i)})
-                        return frequency_dictionary
-                    else:
-                        return None
-            else:
-                return None
-    else:
-        return None
+        if all(isinstance(word, str) for word in tokens):
+            frequency_dictionary = {}
+            for word in tokens:
+                frequency_dictionary.update({word: tokens.count(word)})
+            return frequency_dictionary
 
 
 def get_top_n(frequencies: dict[str, Union[int, float]], top: int) -> Optional[list[str]]:
@@ -109,29 +100,26 @@ def get_top_n(frequencies: dict[str, Union[int, float]], top: int) -> Optional[l
 
     In case of corrupt input arguments, None is returned
     """
-    if isinstance(frequencies, dict) and isinstance(top, int) and isinstance(top, bool) == False and top > 0:
-        for a in frequencies.keys():
-            if isinstance(a, str):
-                for b in frequencies.values():
-                    if isinstance(b, int) or isinstance(b, float):
-                        sort_dictionaries_lst = sorted(frequencies.values(), reverse=True)
-                        sort_dictionaries_dct = {}
-                        for i in sort_dictionaries_lst:
-                            for j in frequencies.keys():
-                                if frequencies[j] == i:
-                                    sort_dictionaries_dct[j] = i
-                        top_lst = []
-                        j = 0
-                        for i in sort_dictionaries_dct.keys():
-                            top_lst += [i]
-                            j += 1
-                            if j == top:
-                                break
-                        return top_lst
-                    else:
-                        return None
-            else:
-                return None
+    if (isinstance(frequencies, dict) and frequencies != {} and isinstance(top, int) and top > 0
+            and isinstance(top, bool) == False):
+        if (all(isinstance(word, str) for word in frequencies.keys())
+                and all(isinstance(freq, int | float) for freq in frequencies.values())):
+            sort_dictionaries_lst = sorted(frequencies.values(), reverse=True)
+            sort_dictionaries_dct = {}
+            for word_one in sort_dictionaries_lst:
+                for word_two in frequencies.keys():
+                    if frequencies[word_two] == word_one:
+                        sort_dictionaries_dct[word_two] = word_one
+            top_lst = []
+            count = 0
+            for word in sort_dictionaries_dct.keys():
+                top_lst += [word]
+                count += 1
+                if count == top:
+                    break
+            return top_lst
+        else:
+            return None
     else:
         return None
 
@@ -153,20 +141,16 @@ def calculate_tf(frequencies: dict[str, int]) -> Optional[dict[str, float]]:
         if None in frequencies.keys():
             return None
         else:
-            for k in frequencies.keys():
-                if isinstance(k, str):
-                    for v in frequencies.values():
-                        if isinstance(v, int):
-                            all_words = 0
-                            for i in frequencies.values():
-                                all_words += i
-                            for k1, v1 in frequencies.items():
-                                frequencies[k1] = v1 / all_words
-                            return frequencies
-                        else:
-                            return None
-                else:
-                    return None
+            if (all(isinstance(word, str) for word in frequencies.keys())
+                    and all(isinstance(freq, int) for freq in frequencies.values())):
+                all_words = 0
+                for freq in frequencies.values():
+                    all_words += freq
+                for key, value in frequencies.items():
+                    frequencies[key] = value / all_words
+                return frequencies
+            else:
+                return None
     else:
         return None
 
@@ -186,28 +170,22 @@ def calculate_tfidf(term_freq: dict[str, float], idf: dict[str, float]) -> Optio
        In case of corrupt input arguments, None is returned
        """
     if isinstance(term_freq, dict) and isinstance(idf, dict):
-        if None in term_freq.keys():
-            return None
+        if (all(isinstance(word, str) for word in term_freq.keys())
+                and all(isinstance(freq, float) for freq in term_freq.values())):
+            if idf == {}:
+                for key, value in term_freq.items():
+                    term_freq[key] = value * math.log(47 / 1)
+                return term_freq
+            else:
+                for key, value in term_freq.items():
+                    for key_two, value_two in term_freq.items():
+                        if key_two in idf.keys():
+                            term_freq[key_two] = value_two * idf[key_two]
+                        else:
+                            term_freq[key_two] = value_two * math.log(47 / 1)
+                    return term_freq
         else:
-            for k, v in term_freq.items():
-                if isinstance(k, str) and isinstance(v, float):
-                    if idf == {}:
-                        for k1, v1 in term_freq.items():
-                            term_freq[k1] = v1 * math.log(47 / 1)
-                        return term_freq
-                    else:
-                        for k0, v0 in term_freq.items():
-                            if isinstance(k0, str) and isinstance(v0, float):
-                                for k2, v2 in term_freq.items():
-                                    if k2 in idf.keys():
-                                        term_freq[k2] = v2 * idf[k2]
-                                    else:
-                                        term_freq[k2] = v2 * math.log(47 / 1)
-                                return term_freq
-                            else:
-                                return None
-                else:
-                    return None
+            return None
 
     else:
         return None
@@ -236,27 +214,29 @@ def calculate_expected_frequency(
                     and all(isinstance(x, int) for x in corpus_freqs.values())):
                 if doc_freqs != {}:
                     final = {}
-                    for k, v in doc_freqs.items():
-                        if k in corpus_freqs.keys():
-                            j = v
-                            k0 = corpus_freqs[k]
-                            l = 0 - j
-                            for i in doc_freqs.values():
-                                l += i
-                            m = 0 - k0
-                            for i in corpus_freqs.values():
-                                m += i
-                            final[k] = (j + k0) * (j + l) / (j + k0 + l + m)
+                    for key, value in doc_freqs.items():
+                        if key in corpus_freqs.keys():
+                            freq_one = value
+                            freq_two = corpus_freqs[key]
+                            total_one = 0 - freq_one
+                            for freq in doc_freqs.values():
+                                total_one += freq
+                            total_two = 0 - freq_two
+                            for freq in corpus_freqs.values():
+                                total_two += freq
+                            final[key] = ((freq_one + freq_two) * (freq_one + total_one) /
+                                          (freq_one + freq_two + total_one + total_two))
                         else:
-                            j = v
-                            k0 = 0
-                            l = 0 - j
-                            for i in doc_freqs.values():
-                                l += i
-                            m = 0
-                            for i in corpus_freqs.values():
-                                m += i
-                            final[k] = (j + k0) * (j + l) / (j + k0 + l + m)
+                            freq_one = value
+                            freq_two = 0
+                            total_one = 0 - freq_one
+                            for freq in doc_freqs.values():
+                                total_one += freq
+                            total_two = 0
+                            for freq in corpus_freqs.values():
+                                total_two += freq
+                            final[key] = ((freq_one + freq_two) * (freq_one + total_one) /
+                                          (freq_one + freq_two + total_one + total_two))
                     return final
                 else:
                     return None
@@ -285,15 +265,15 @@ def calculate_chi_values(expected: dict[str, float], observed: dict[str, int]) -
     In case of corrupt input arguments, None is returned
     """
     if isinstance(expected, dict) and isinstance(observed, dict):
-        if (all(isinstance(x, float) for x in expected.values())
-                and all(isinstance(x, int) for x in observed.values())):
-            if (all(isinstance(x, str) for x in expected.keys())
-                    and all(isinstance(x, str) for x in observed.keys())):
+        if (all(isinstance(freq, float) for freq in expected.values())
+                and all(isinstance(freq, int) for freq in observed.values())):
+            if (all(isinstance(word, str) for word in expected.keys())
+                    and all(isinstance(word, str) for word in observed.keys())):
                 if expected != {} and observed != {}:
                     final_dict = {}
-                    for k, v in expected.items():
-                        if k in observed.keys():
-                            final_dict[k] = pow((observed[k] - v), 2)/v
+                    for key, value in expected.items():
+                        if key in observed.keys():
+                            final_dict[key] = pow((observed[key] - value), 2)/value
                     return final_dict
                 else:
                     return None
@@ -324,19 +304,19 @@ def extract_significant_words(chi_values: dict[str, float], alpha: float) -> Opt
     In case of corrupt input arguments, None is returned
     """
     if isinstance(chi_values, dict) and isinstance(alpha, float):
-        if (all(isinstance(x, str) for x in chi_values.keys())
-                and all(isinstance(x, float) for x in chi_values.values()) and chi_values != {}):
+        if (all(isinstance(word, str) for word in chi_values.keys())
+                and all(isinstance(freq, float) for freq in chi_values.values()) and chi_values != {}):
             if alpha == 0.05 or alpha == 0.01 or alpha == 0.001:
                 final_dict = {}
                 if alpha == 0.05:
-                    a = 3.842
+                    digit = 3.842
                 if alpha == 0.01:
-                    a = 6.635
+                    digit = 6.635
                 if alpha == 0.001:
-                    a = 10.828
-                for k, v in chi_values.items():
-                    if v > a:
-                        final_dict[k] = v
+                    digit = 10.828
+                for key, value in chi_values.items():
+                    if value > digit:
+                        final_dict[key] = value
                 return final_dict
             else:
                 return None
