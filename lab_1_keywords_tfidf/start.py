3534e6732a6ce47d37a850f6677ead8fb5db0ad0
Chris <kristinanigamedzanova@gmail.com>
2022-09-25 12:29:10 +0300

    checked tests

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
 
     print(get_top_n(chi_values, 10))
 
-    RESULT = get_top_n(chi_values, 10))
+    RESULT = get_top_n(chi_values, 10)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Keywords are not extracted'
 
