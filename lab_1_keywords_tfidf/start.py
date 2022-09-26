41026921ddd491702dc951a69851e0e01c72ced8
Полина Доможирова <polinadomozirova@Polinas-MacBook-Air.local>
2022-09-26 12:28:35 +0300

    changes in start.py

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -3,7 +3,8 @@ Frequency-driven keyword extraction starter
 """
 import json
 from pathlib import Path
-import main
+from lab_1_keywords_tfidf.main import (clean_and_tokenize, remove_stop_words, calculate_frequencies, get_top_n, calculate_tf, calculate_tfidf)
+
 
 
 if __name__ == "__main__":
@@ -17,26 +18,48 @@ if __name__ == "__main__":
     with open(TARGET_TEXT_PATH, 'r', encoding='utf-8') as file:
         target_text = file.read()
 
+    tokens = clean_and_tokenize(target_text)
+    print(tokens)
+
     # reading list of stop words
     STOP_WORDS_PATH = ASSETS_PATH / 'stop_words.txt'
     with open(STOP_WORDS_PATH, 'r', encoding='utf-8') as file:
         stop_words = file.read().split('\n')
+
+    rem_tokens = remove_stop_words(tokens, stop_words)
+    print(rem_tokens)
+
+    frequencies = calculate_frequencies(rem_tokens)
+    print(frequencies)
+
+    top_10_words = get_top_n(frequencies, 10)
+    print(top_10_words)
+
+    tf = calculate_tf(frequencies)
+    print(tf)
 
     # reading IDF scores for all tokens in the corpus of H.C. Andersen tales
     IDF_PATH = ASSETS_PATH / 'IDF.json'
     with open(IDF_PATH, 'r', encoding='utf-8') as file:
         idf = json.load(file)
 
+
+    tfidf = calculate_tfidf(tf, idf)
+    print(tfidf)
+
+    top_10_tfidf = get_top_n(tfidf, 10)
+    print(top_10_tfidf)
+
     # reading frequencies for all tokens in the corpus of H.C. Andersen tales
     CORPUS_FREQ_PATH = ASSETS_PATH / 'corpus_frequencies.json'
     with open(CORPUS_FREQ_PATH, 'r', encoding='utf-8') as file:
         corpus_freqs = json.load(file)
 
+
+
+
     RESULT = None
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Keywords are not extracted'
 
-print(main.clean_and_tokenize(target_text))
-tokens = main.clean_and_tokenize(target_text)
-print(main.remove_stop_words(tokens, stop_words))
-tokens = main.remove_stop_words(tokens, stop_words)
+
