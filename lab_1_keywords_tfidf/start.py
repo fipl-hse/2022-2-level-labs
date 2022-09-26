82736b6173eadc3d76ff43839676eb56c62fbeeb
Полина Доможирова <polinadomozirova@Polinas-MacBook-Air.local>
2022-09-26 12:35:36 +0300

    changes in start.py 2

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
 
 
 
-    RESULT = None
+    RESULT = get_top_n
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Keywords are not extracted'
 
