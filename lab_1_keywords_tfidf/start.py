977cb9eda545af352d53903f27dd8589344015b3
Полина Доможирова <polinadomozirova@Polinas-MacBook-Air.local>
2022-09-26 15:12:19 +0300

    some changes

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -3,8 +3,12 @@ Frequency-driven keyword extraction starter
 """
 import json
 from pathlib import Path
-from lab_1_keywords_tfidf.main import clean_and_tokenize, remove_stop_words, calculate_frequencies, get_top_n, calculate_tf, \
-    calculate_tfidf, calculate_expected_frequency
+from lab_1_keywords_tfidf.main import (clean_and_tokenize,
+                                       remove_stop_words,
+                                       calculate_frequencies,
+                                       get_top_n,
+                                       calculate_tf,
+                                       calculate_tfidf)
 
 
 
@@ -59,7 +63,7 @@ if __name__ == "__main__":
 
 
 
-    RESULT = get_top_n
+    RESULT = top_10_tfidf
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Keywords are not extracted'
 
