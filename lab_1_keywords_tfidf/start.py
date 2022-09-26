58d4737b1c5d95f5883985d7db2c973cee234407
Ola Pershina <you@example.com>
2022-09-26 15:31:54 +0300

    some changes for check type

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -55,7 +55,6 @@ if __name__ == "__main__":
 
     if NO_STOP_WORDS:
         FREQ_DICT = calculate_frequencies(NO_STOP_WORDS)
-        print(get_top_n(FREQ_DICT, 8))
 
     if FREQ_DICT:
         TF_DICT = calculate_tf(FREQ_DICT)
