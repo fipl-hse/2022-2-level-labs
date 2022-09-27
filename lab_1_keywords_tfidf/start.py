caa5cae624f43f26950d5c4259f6e65822f2be0e
Bushueva Nataliya <natasha.boo.nb@gmail.com>
2022-09-27 21:56:30 +0300

    12

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -20,20 +20,24 @@ if __name__ == "__main__":
     with open(STOP_WORDS_PATH, 'r', encoding='utf-8') as file:
         stop_words = file.read().split('\n')
         tokens = clean_and_tokenize(target_text)
-        tokens = remove_stop_words(tokens, stop_words)
-        frequencies = calculate_frequencies(tokens)
+        if tokens is not None and stop_words is not None:
+            tokens = remove_stop_words(tokens, stop_words)
+            frequencies = calculate_frequencies(tokens)
     # reading IDF scores for all tokens in the corpus of H.C. Andersen tales
     IDF_PATH = ASSETS_PATH / 'IDF.json'
     with open(IDF_PATH, 'r', encoding='utf-8') as file:
         idf = json.load(file)
-        term_freq = calculate_tf(frequencies)
+        if frequencies is not None:
+            term_freq = calculate_tf(frequencies)
 
     # reading frequencies for all tokens in the corpus of H.C. Andersen tales
     CORPUS_FREQ_PATH = ASSETS_PATH / 'corpus_frequencies.json'
     with open(CORPUS_FREQ_PATH, 'r', encoding='utf-8') as file:
         corpus_freqs = json.load(file)
-        tfidf = calculate_tfidf(term_freq, idf)
-    RESULT = get_top_n(tfidf, 10)
+        if term_freq is not None and idf is not None:
+            tfidf = calculate_tfidf(term_freq, idf)
+            if frequencies.get is not None:
+                RESULT = get_top_n(tfidf, 10)
     print("Top 10 most common words in the text: ", RESULT)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Keywords are not extracted'
