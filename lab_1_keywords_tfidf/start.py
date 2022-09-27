cd83bc79e395c82ce8d1015ce0302c7621ee2e89
chezar0 <egorow4.a@yandex.ru>
2022-09-27 12:30:48 +0300

    start final changes??

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -33,13 +33,17 @@ if __name__ == "__main__":
         corpus_freqs = json.load(file)
 
     RESULT = None
-
-    tokens = clean_and_tokenize(target_text)
-    cleaned_tokens = remove_stop_words(tokens, stop_words)
-    frequencies = calculate_frequencies(cleaned_tokens)
-    term_freq = calculate_tf(frequencies)
-    tfidf_res = calculate_tfidf(term_freq, idf)
-    print(get_top_n(tfidf_res, 10))
+    if target_text:
+        tokens = clean_and_tokenize(target_text)
+    if tokens or stop_words:
+        cleaned_tokens = remove_stop_words(tokens, stop_words)
+    if cleaned_tokens:
+        frequencies = calculate_frequencies(cleaned_tokens)
+    if frequencies:
+        term_freq = calculate_tf(frequencies)
+    if term_freq or idf:
+        tfidf_res = calculate_tfidf(term_freq, idf)
+    RESULT = print(get_top_n(tfidf_res, 10))
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Keywords are not extracted'
