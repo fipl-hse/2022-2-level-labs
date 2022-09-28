91f87ad40e3b1ef873abd6e1e811931aaddf39a1
Viktoria Teterina <vsteterina@edu.hse.ru>
2022-09-29 00:47:06 +0300

    changed main.p

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -2,22 +2,12 @@
 Lab 1
 Extract keywords based on frequency related metrics
 """
+import math
 from typing import Optional, Union, Any
 import string
 from string import punctuation
 
-
-
 def clean_and_tokenize(text: str) -> Optional[list[str]]:
-    if isistance (text, str):
-        text = text.lower().strip()
-        for symbol in punctuation:
-            if symbol in text:
-                text = text.replace(symbol, ' ')
-            tokens = text.split()
-            return tokens
-    else:
-        return None
     """
     Removes punctuation, casts to lowercase, splits into tokens
 
@@ -29,18 +19,19 @@ def clean_and_tokenize(text: str) -> Optional[list[str]]:
 
     In case of corrupt input arguments, None is returned
     """
+    if  not isistance (text, str):
+        return None
+    text = text.lower().strip()
+    punctuation = '''!@#$%^&*()_-+=[]{};:'\|,<.>/?"'''
+    for symbol in punctuation:
+        if symbol in text:
+            text = text.replace(symbol, '')
+    tokens = text.split()
+    return tokens
     pass
 
 
 def remove_stop_words(tokens: list[str], stop_words: list[str]) -> Optional[list[str]]:
-    if isistance(tokens, list) and isinstance(stop_words, list):
-        for stop_word in stop_words:
-            while stop_word in tokens:
-                tokens.remove(stop_word)
-                return tokens
-    else:
-        return None
-
     """
     Excludes stop words from the token sequence
 
@@ -53,19 +44,22 @@ def remove_stop_words(tokens: list[str], stop_words: list[str]) -> Optional[list
 
     In case of corrupt input arguments, None is returned
     """
+    if not (isistance(tokens, list) and isinstance(stop_words, list)):
+        return None
+    for stop_word in stop_words:
+        while stop_word in tokens:
+            tokens.remove(stop_word)
+    return tokens
+
+
+def check(massive, type_name) -> Optional[bool]:
+    if not (massive and all(isisstance(el, type_name) for el in massive)):
+        return False
+    return True
     pass
 
 
 def calculate_frequencies(tokens: list[str]) -> Optional[dict[str, int]]:
-    frequences = {}
-    if isinstance(tokens, list):
-        for token in tokens:
-            occurance_number = token.count(token)
-            frequences[token] = occurance_number
-        return frequences
-    else:
-        return None
-
     """
     Composes a frequency dictionary from the token sequence
 
@@ -77,29 +71,56 @@ def calculate_frequencies(tokens: list[str]) -> Optional[dict[str, int]]:
 
     In case of corrupt input arguments, None is returned
     """
+    frequences = {}
+    if not (isinstance(tokens, list)):
+        return None
+    for token in tokens:
+        occurance_number = token.count(token)
+        frequences[token] = occurance_number
+    return frequences
     pass
 
-
-def get_top_n(frequencies: dict[str, Union[int, float]], top: int) -> Optional[list[str]]:
-    if isinstance(frequencies, dict) and isinstance(top, int) and top >=1:
+    def get_top_n(frequencies: dict[str, Union[int, float]], top: int) -> Optional[list[str]]:
+    # """"
+    # Extracts a certain number of most frequent tokens
+    #
+    # Parameters:
+    # frequencies (Dict): A dictionary with tokens and
+    # its corresponding frequency values
+    # top (int): Number of token to extract
+    #
+    # Returns:
+    # List[str]: Sequence of specified length
+    # consisting of tokens with the largest frequency
+    #
+    # In case of corrupt input arguments, None is returned
+    # """
+        if not (isinstance(frequencies, dict) and top != int):
+            return None
         key_list = list(frequencies.keys())
         value_list = list(frequencies.values())
+        if check(key_list, str) and check(value_list, int) or check(value_list, float):
+            value_list.sort(reserve=True)
+            top_list = []
 
+            def tokens(top_list: list[str], frequences: dict[str, Union[int, float]], value_list, float):
+                for word, number in frequences.items():
+                    if word not in top_list and number == value:
+                        return word
 
-    """
-    Extracts a certain number of most frequent tokens
-
-    Parameters:
-    frequencies (Dict): A dictionary with tokens and
-    its corresponding frequency values
-    top (int): Number of token to extract
-
-    Returns:
-    List[str]: Sequence of specified length
-    consisting of tokens with the largest frequency
-
-    In case of corrupt input arguments, None is returned
-    """
+    def get_list(length: int):
+        if not isistance(length, int) or length is True:
+            return None
+        for i in range(length):
+            token = tokens(top_list, frequences, int(value_list[i]))
+            top_list.append(token)
+        return top_list
+    if len(top_list) != top:
+        if top > len(key_list):
+            top_list = get_list(len(key_list))
+        else:
+            top_list = get_list(top)
+    return top_list
     pass
 
 
