7b40d4508967ba7da1563310c8a7f1b0bb714f25
pdshh <pdshamanina@gmail.com>
2022-09-29 19:17:00 +0300

    fix

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -43,9 +43,6 @@ if __name__ == "__main__":
         frequencies = calculate_frequencies(tokens_no_sw)
 
     if frequencies:
-        most_frequent = get_top_n(frequencies, 10)
-
-    if frequencies:
         term_freq = calculate_tf(frequencies)
 
     if term_freq:
