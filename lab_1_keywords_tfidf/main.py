531a18897659a69b6773c25ef1a1697606d1971e
Egor <egordzyumenko@yandex.ru>
2022-09-30 12:00:10 +0300

    new line 2nd try

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -61,7 +61,6 @@ def remove_stop_words(tokens: list[str], stop_words: list[str]) -> Optional[list
             type_of_elements(tokens, str) and type_of_elements(stop_words, str)
             ):
         return None
-    
     tokens_cleaned = [key_word for key_word in tokens if key_word not in stop_words]
     return tokens_cleaned
 
