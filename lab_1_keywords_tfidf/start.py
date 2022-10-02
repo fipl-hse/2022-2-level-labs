0b153e2d41398da770dba18ddd653fbc6aa2525b
artyomtugaryov <artyomtugaryov@users.noreply.github.com>
2022-10-02 17:41:59 +0300

    checkout labs from the origin repository

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -14,13 +14,6 @@ from lab_1_keywords_tfidf.main import (
     calculate_chi_values
 )
 
-from lab_1_keywords_tfidf.main import clean_and_tokenize, \
-                                      remove_stop_words, \
-                                      calculate_frequencies, \
-                                      get_top_n, \
-                                      calculate_tf, \
-                                      calculate_tfidf
-
 if __name__ == "__main__":
 
     # finding paths to the necessary utils
