1710ad6d218514421a8a073b2973a1f25ea83b1b
artyomtugaryov <artyomtugaryov@users.noreply.github.com>
2022-10-02 17:41:47 +0300

    checkout labs from the origin repository

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -65,8 +65,3 @@ if __name__ == "__main__":
         RESULT = get_top_n(chi_dict, 10)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Keywords are not extracted'
-
-
-
-
-
