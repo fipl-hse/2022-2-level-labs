96f5121f1fe906cbece2068a21eeff49c0906c4a
KatherineGD <ek.grigorieva@inbox.ru>
2022-10-07 11:36:23 +0300

    changes

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -140,3 +140,78 @@ def get_top_n(frequencies: dict[str, Union[int, float]], top: int) -> Optional[l
     if not (checking_dict and check_positive_int(top)):
         return None
     return sorted(frequencies.keys(), key=lambda key: frequencies[key], reverse=True)[:top]
+
+
+def calculate_tf(frequencies: dict[str, int]) -> Optional[dict[str, float]]:
+    """
+    Calculates Term Frequency score for each word in a token sequence
+    based on the raw frequency
+    Parameters:
+    frequencies (Dict): Raw number of occurrences for each of the tokens
+    Returns:
+    dict: A dictionary with tokens and corresponding term frequency score
+    In case of corrupt input arguments, None is returned
+    """
+    pass
+
+
+def calculate_tfidf(term_freq: dict[str, float], idf: dict[str, float]) -> Optional[dict[str, float]]:
+    """
+    Calculates TF-IDF score for each of the tokens
+    based on its TF and IDF scores
+    Parameters:
+    term_freq (Dict): A dictionary with tokens and its corresponding TF values
+    idf (Dict): A dictionary with tokens and its corresponding IDF values
+    Returns:
+    Dict: A dictionary with tokens and its corresponding TF-IDF values
+    In case of corrupt input arguments, None is returned
+    """
+    pass
+
+
+def calculate_expected_frequency(
+    doc_freqs: dict[str, int], corpus_freqs: dict[str, int]
+) -> Optional[dict[str, float]]:
+    """
+    Calculates expected frequency for each of the tokens based on its
+    Term Frequency score for both target document and general corpus
+    Parameters:
+    doc_freqs (Dict): A dictionary with tokens and its corresponding number of occurrences in document
+    corpus_freqs (Dict): A dictionary with tokens and its corresponding number of occurrences in corpus
+    Returns:
+    Dict: A dictionary with tokens and its corresponding expected frequency
+    In case of corrupt input arguments, None is returned
+    """
+    pass
+
+
+def calculate_chi_values(expected: dict[str, float], observed: dict[str, int]) -> Optional[dict[str, float]]:
+    """
+    Calculates chi-squared value for the tokens
+    based on their expected and observed frequency rates
+    Parameters:
+    expected (Dict): A dictionary with tokens and
+    its corresponding expected frequency
+    observed (Dict): A dictionary with tokens and
+    its corresponding observed frequency
+    Returns:
+    Dict: A dictionary with tokens and its corresponding chi-squared value
+    In case of corrupt input arguments, None is returned
+    """
+    pass
+
+
+def extract_significant_words(chi_values: dict[str, float], alpha: float) -> Optional[dict[str, float]]:
+    """
+    Select those tokens from the token sequence that
+    have a chi-squared value greater than the criterion
+    Parameters:
+    chi_values (Dict): A dictionary with tokens and
+    its corresponding chi-squared value
+    alpha (float): Level of significance that controls critical value of chi-squared metric
+    Returns:
+    Dict: A dictionary with significant tokens
+    and its corresponding chi-squared value
+    In case of corrupt input arguments, None is returned
+    """
+    pass
