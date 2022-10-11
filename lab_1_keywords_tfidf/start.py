a2ffda14f504845d2dea4f6c2d430d8a47136e50
Bushueva Nataliya <natasha.boo.nb@gmail.com>
2022-10-11 15:10:33 +0300

    Revert lab1

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -15,6 +15,7 @@ from lab_1_keywords_tfidf.main import (
 )
 
 if __name__ == "__main__":
+
     # finding paths to the necessary utils
     PROJECT_ROOT = Path(__file__).parent
     ASSETS_PATH = PROJECT_ROOT / 'assets'
@@ -23,24 +24,16 @@ if __name__ == "__main__":
     TARGET_TEXT_PATH = ASSETS_PATH / 'Дюймовочка.txt'
     with open(TARGET_TEXT_PATH, 'r', encoding='utf-8') as file:
         target_text = file.read()
+
     # reading list of stop words
     STOP_WORDS_PATH = ASSETS_PATH / 'stop_words.txt'
     with open(STOP_WORDS_PATH, 'r', encoding='utf-8') as file:
         stop_words = file.read().split('\n')
-        if target_text is not None:
-            tokens = clean_and_tokenize(target_text)
-        if tokens is not None and stop_words is not None:
-            tokens = remove_stop_words(tokens, stop_words)
-        if tokens is not None:
-            frequencies = calculate_frequencies(tokens)
+
     # reading IDF scores for all tokens in the corpus of H.C. Andersen tales
     IDF_PATH = ASSETS_PATH / 'IDF.json'
     with open(IDF_PATH, 'r', encoding='utf-8') as file:
         idf = json.load(file)
-        if frequencies is not None:
-            term_freq = calculate_tf(frequencies)
-            if term_freq is not None and idf is not None:
-                tfidf = calculate_tfidf(term_freq, idf)
 
     # reading frequencies for all tokens in the corpus of H.C. Andersen tales
     CORPUS_FREQ_PATH = ASSETS_PATH / 'corpus_frequencies.json'
