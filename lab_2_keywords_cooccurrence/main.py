ef4d975ccc1c1d1634f48e4f05e1b233beffa199
zinaidagaraeva <zinag2003@gmail.com>
2022-10-14 10:36:22 +0300

    file main is changed for creating a new pull request

--- lab_2_keywords_cooccurrence/main.py
+++ lab_2_keywords_cooccurrence/main.py
@@ -18,6 +18,7 @@ def extract_phrases(text: str) -> Optional[Sequence[str]]:
     In case of corrupt input arguments, None is returned
     """
     pass
+#####
 
 
 def extract_candidate_keyword_phrases(phrases: Sequence[str], stop_words: Sequence[str]) -> Optional[KeyPhrases]:
