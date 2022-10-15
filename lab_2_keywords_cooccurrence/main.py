a7a9bbcdf73af2a1267d1829d8ce39f06bc2a885
danind <evolved.dinosaur@gmail.com>
2022-10-15 19:42:18 +0300

    for mark 10: changes according to code_style

--- lab_2_keywords_cooccurrence/main.py
+++ lab_2_keywords_cooccurrence/main.py
@@ -299,7 +299,7 @@ def extract_keyword_phrases(target_text: str, stop_words: Optional[Sequence] = N
     """
     Using previous functions, extracts keyword phrases from user's text
     """
-    candidates, keyword_phrases_with_stop_words = [None for notdef in range(2)]
+    candidates = None
     extracted_phrases = extract_phrases(target_text)
     if stop_words is None:
         stop_words = generate_stop_words(target_text, max_length)
@@ -316,7 +316,7 @@ def calculate_cumulative_score(target_text: str, stop_words: Optional[Sequence] 
     Using previous functions, calculates cumulative score of tokens in user's text
     """
     candidates, content_words, word_degrees, word_scores, \
-        keyword_phrases_with_stop_words, cumulative_score_with_stop_words = [None for notdef in range(6)]
+        cumulative_score_with_stop_words = [None for notdef in range(6)]
     extracted_phrases = extract_phrases(target_text)
     if stop_words is None:
         stop_words = generate_stop_words(target_text, max_length)
