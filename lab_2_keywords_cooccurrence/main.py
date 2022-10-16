a7cf68bf2782c82d75ef6b61a7d75f4e0cbcb138
Yulia Lopatko <lopatko.julia3@gmail.com>
2022-10-16 22:41:55 +0300

    i cant see

--- lab_2_keywords_cooccurrence/main.py
+++ lab_2_keywords_cooccurrence/main.py
@@ -226,9 +226,9 @@ def extract_candidate_keyword_phrases_with_adjoining(candidate_keyword_phrases: 
                 for index in range(len(phrase)):
                     phrase_with_stop_words = phrase.lower().split()[index:index
                     + len(k[0].split()) + len(k[1].split()) + 1]
-                    if set(possible_phrase).issubset(set(phrase_with_stop_words)):
+                    if set(possible_phrase).issubset(phrase_with_stop_words):
                         candidates_with_adjoining.append(tuple(phrase_with_stop_words))
-    for phrase in candidates_with_adjoining:
+    for phrase in set(candidates_with_adjoining):
         candidates_with_adjoining.remove(phrase)
     return list(set(candidates_with_adjoining))
 
