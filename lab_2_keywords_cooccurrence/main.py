0eb332bf6aa4bde1b3d3b55d61a21287d906a96e
Yulia Lopatko <lopatko.julia3@gmail.com>
2022-10-16 23:42:10 +0300

    my eyes

--- lab_2_keywords_cooccurrence/main.py
+++ lab_2_keywords_cooccurrence/main.py
@@ -185,7 +185,9 @@ def get_top_n(keyword_phrases_with_scores: Mapping[KeyPhrase, float],
                  reverse=True)
     for phrase in top:
         if len(phrase) <= max_length:
+            phrase = list(phrase)
             phrase = " ".join(phrase)
+            top_keyword_phrases = list(top_keyword_phrases)
             top_keyword_phrases.append(phrase)
     return top_keyword_phrases[:top_n]
 
