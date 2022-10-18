ac0c8b932fe5ed1b3e2776aa67c83f12c8c866b4
chezar0 <egorow4.a@yandex.ru>
2022-10-18 12:22:46 +0300

    start fix

--- lab_2_keywords_cooccurrence/start.py
+++ lab_2_keywords_cooccurrence/start.py
@@ -63,7 +63,9 @@ if __name__ == "__main__":
     if cum_score:
         top_list = get_top_n(cum_score, 10, 3)
     if candidate_keyword_phrases:
-        RESULT = extract_candidate_keyword_phrases_with_adjoining(candidate_keyword_phrases, word_scores, stop_words)
-        print(RESULT)
+        ajoin_phrases = extract_candidate_keyword_phrases_with_adjoining(candidate_keyword_phrases, phrases)
+    if ajoin_phrases:
+        RESULT = calculate_cumulative_score_for_candidates_with_stop_words(ajoin_phrases, word_scores, stop_words)
+    print(RESULT)
 
     assert RESULT, 'Keywords are not extracted'
