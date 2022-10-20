0f8b8b5e3233d1c83c5170aab859cb9ccba85f5c
Andrei K <logerk3@gmail.com>
2022-10-20 19:14:03 +0300

    add tests, assets and description

---
+++ lab_3_keywords_textrank/start.py
@@ -0,0 +1,38 @@
+"""
+TextRank keyword extraction starter
+"""
+
+from pathlib import Path
+from string import punctuation
+import sys
+from time import time
+
+from lab_3_keywords_textrank.main import (
+    TextPreprocessor,
+    TextEncoder,
+    VanillaTextRank,
+    PositionBiasedTextRank,
+    AdjacencyMatrixGraph,
+    EdgeListGraph
+)
+
+# finding paths to the necessary utils
+PROJECT_ROOT = Path(__file__).parent
+ASSETS_PATH = PROJECT_ROOT / 'assets'
+
+# reading the text from which keywords are going to be extracted
+TARGET_TEXT_PATH = ASSETS_PATH / 'article.txt'
+with open(TARGET_TEXT_PATH, 'r', encoding='utf-8') as file:
+    text = file.read()
+
+# reading list of stop words
+STOP_WORDS_PATH = ASSETS_PATH / 'stop_words.txt'
+with open(STOP_WORDS_PATH, 'r', encoding='utf-8') as file:
+    stop_words = tuple(file.read().split('\n'))
+
+
+if __name__ == "__main__":
+
+    RESULT = None
+
+    assert RESULT, 'Keywords are not extracted'
