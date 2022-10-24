e6d516ad786da3872e9ffa011f990d29d84d1cef
Artem Tugarev <amtugarev@avito.ru>
2022-10-24 15:12:21 +0300

    checkout labs from the origin repository

--- lab_2_keywords_cooccurrence/start.py
+++ lab_2_keywords_cooccurrence/start.py
@@ -20,37 +20,6 @@ def read_target_text(file_path: Path) -> str:
         return target_text_file.read()
 
 
-def text_processing(text: str, list_of_stop_word: Sequence[str]) -> None:
-    candidate_keyword_phrases, frequencies_for_content_words, word_degrees, word_scores, cumulative_score, \
-        candidate_keyword_phrases_with_adjoining = [None for _ in range(6)]
-    phrases = extract_phrases(text)
-    if phrases:
-        candidate_keyword_phrases = extract_candidate_keyword_phrases(phrases, list_of_stop_word)
-
-    if candidate_keyword_phrases:
-        frequencies_for_content_words = calculate_frequencies_for_content_words(candidate_keyword_phrases)
-
-    if candidate_keyword_phrases and frequencies_for_content_words:
-        word_degrees = calculate_word_degrees(candidate_keyword_phrases, list(frequencies_for_content_words.keys()))
-
-    if word_degrees and frequencies_for_content_words:
-        word_scores = calculate_word_scores(word_degrees, frequencies_for_content_words)
-
-    if candidate_keyword_phrases and word_scores:
-        cumulative_score = calculate_cumulative_score_for_candidates(candidate_keyword_phrases, word_scores)
-
-    if cumulative_score:
-        print(get_top_n(cumulative_score, 5, 3))
-
-    if candidate_keyword_phrases and phrases:
-        candidate_keyword_phrases_with_adjoining = extract_candidate_keyword_phrases_with_adjoining(
-                                                   candidate_keyword_phrases, phrases)
-
-    if candidate_keyword_phrases_with_adjoining and word_scores and list_of_stop_word:
-        calculate_cumulative_score_for_candidates_with_stop_words(candidate_keyword_phrases_with_adjoining, word_scores,
-                                                                  list_of_stop_word)
-
-
 if __name__ == "__main__":
     # finding paths to the necessary utils
     PROJECT_ROOT = Path(__file__).parent
@@ -101,15 +70,4 @@ if __name__ == "__main__":
 
     RESULT = UNKNOWN_PROCESSED
 
-    LOADED_STOP_WORDS = load_stop_words(ASSETS_PATH / 'stopwords.json')
-
-    if LOADED_STOP_WORDS:
-        text_processing(read_target_text(ASSETS_PATH / 'polish.txt'), LOADED_STOP_WORDS['pl'])
-
-    UNKNOWN_TEXT = read_target_text(ASSETS_PATH / 'unknown.txt')
-    STOP_WORDS_IN_UNKNOWN_TEXT = generate_stop_words(UNKNOWN_TEXT, 10)
-    if STOP_WORDS_IN_UNKNOWN_TEXT:
-        text_processing(UNKNOWN_TEXT, STOP_WORDS_IN_UNKNOWN_TEXT)
-
-    RESULT = True
     assert RESULT, 'Keywords are not extracted'
