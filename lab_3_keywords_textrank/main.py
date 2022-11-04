639e802af07477c19e859ae6763fae3bbe5ec6f8
Pavel Shkunov <pvlshkunov@gmail.com>
2022-11-04 16:03:58 +0300

    check type

--- lab_3_keywords_textrank/main.py
+++ lab_3_keywords_textrank/main.py
@@ -1019,4 +1019,3 @@ class KeywordExtractionBenchmark:
             for i in self.report:
                 file.write(','.join([i, *map(str, self.report[i].values())]))
                 file.write('\n')
-
