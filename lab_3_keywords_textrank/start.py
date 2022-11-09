d5b0d54e4b53247a2759b622ffb79348d3831886
cybern00dle <koshech_noskich@mail.ru>
2022-11-09 23:05:23 +0300

    step 6 added

--- lab_3_keywords_textrank/start.py
+++ lab_3_keywords_textrank/start.py
@@ -3,6 +3,9 @@ TextRank keyword extraction starter
 """
 
 from pathlib import Path
+from string import punctuation
+from lab_3_keywords_textrank.main import extract_pairs, TextPreprocessor, TextEncoder, AdjacencyMatrixGraph, \
+    VanillaTextRank
 
 
 if __name__ == "__main__":
@@ -20,6 +23,24 @@ if __name__ == "__main__":
     STOP_WORDS_PATH = ASSETS_PATH / 'stop_words.txt'
     with open(STOP_WORDS_PATH, 'r', encoding='utf-8') as file:
         stop_words = tuple(file.read().split('\n'))
+
+    # step 3
+    preprocessor = TextPreprocessor(stop_words, tuple(punctuation))
+    preprocessed_text = preprocessor.preprocess_text(text)
+    encoder = TextEncoder()
+    tokens = encoder.encode(preprocessed_text)
+    if tokens:
+        print(extract_pairs(tokens, 3))
+
+    # step 6
+    adjacency_matrix_graph = AdjacencyMatrixGraph()
+    if tokens:
+        adjacency_matrix_graph.fill_from_tokens(tokens, 3)
+        adjacency_matrix_graph.fill_positions(tokens)
+        adjacency_matrix_graph.calculate_position_weights()
+    vanilla_rank_adjacency = VanillaTextRank(adjacency_matrix_graph)
+    vanilla_rank_adjacency.train()
+    print(encoder.decode(vanilla_rank_adjacency.get_top_keywords(10)))
 
     RESULT = None
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
