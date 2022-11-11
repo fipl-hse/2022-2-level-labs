ec485a75d6cae84edd23eae6c831a769ef0cdfc8
Timur20033 <timurorigami@mail.ru>
2022-11-11 06:01:30 +0300

    start 8

--- lab_3_keywords_textrank/start.py
+++ lab_3_keywords_textrank/start.py
@@ -5,7 +5,7 @@ import string
 from pathlib import Path
 from string import punctuation
 from lab_3_keywords_textrank.main import (TextPreprocessor, TextEncoder, extract_pairs,
-                                          AdjacencyMatrixGraph, VanillaTextRank)
+                                          AdjacencyMatrixGraph, VanillaTextRank, EdgeListGraph, PositionBiasedTextRank)
 
 if __name__ == "__main__":
 
@@ -25,18 +25,56 @@ if __name__ == "__main__":
 
     RESULT = None
     pairs = None
-    preprocessor = TextPreprocessor(stop_words, tuple(string.punctuation))
-    tokens = preprocessor.preprocess_text(text)
+
+    # mark 4
+    preprocessed_text = TextPreprocessor(stop_words, tuple(punctuation)).preprocess_text(text)
     encoder = TextEncoder()
-    numbers = encoder.encode(tokens)
-    graph = AdjacencyMatrixGraph()
-    if numbers:
-        graph.fill_from_tokens(numbers, 5)
-    rank = VanillaTextRank(graph)
-    rank.train()
-    top = rank.get_top_keywords(10)
-    RESULT = encoder.decode(top)
-    print(RESULT)
+    tokens = encoder.encode(preprocessed_text)
+    if tokens:
+        pairs = extract_pairs(tokens, 3)
+    print(pairs)
+
+    # mark 6
+    matrix_graph = AdjacencyMatrixGraph()
+    if tokens:
+        matrix_graph.fill_from_tokens(tokens, 3)
+        matrix_graph.fill_positions(tokens)
+    matrix_graph.calculate_position_weights()
+
+    vanilla_matrix = VanillaTextRank(matrix_graph)
+    vanilla_matrix.train()
+    top_vanilla_matrix = vanilla_matrix.get_top_keywords(10)
+    if top_vanilla_matrix:
+        res1 = encoder.decode(top_vanilla_matrix)
+        print(res1)
+
+    edge_graph = EdgeListGraph()
+    if tokens:
+        edge_graph.fill_from_tokens(tokens, 3)
+        edge_graph.fill_positions(tokens)
+    edge_graph.calculate_position_weights()
+
+    vanilla_edge = VanillaTextRank(edge_graph)
+    vanilla_edge.train()
+    top_vanilla_edge = vanilla_edge.get_top_keywords(10)
+    if top_vanilla_edge:
+        res2 = encoder.decode(top_vanilla_edge)
+        print(res2)
+
+    # mark 8
+    biased_matrix = PositionBiasedTextRank(matrix_graph)
+    biased_matrix.train()
+    top_biased_matrix = biased_matrix.get_top_keywords(10)
+    if top_biased_matrix:
+        res3 = encoder.decode(top_biased_matrix)
+        print(res3)
+
+    biased_edge = PositionBiasedTextRank(edge_graph)
+    biased_edge.train()
+    top_biased_edge = biased_edge.get_top_keywords(10)
+    if top_biased_edge:
+        RESULT = encoder.decode(top_biased_edge)
+        print(RESULT)
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Keywords are not extracted'
