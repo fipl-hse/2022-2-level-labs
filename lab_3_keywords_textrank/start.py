098b90dd69ef9a85df9f56c7431aa849db164711
danind <evolved.dinosaur@gmail.com>
2022-11-21 13:01:52 +0300

    for mark 10: start.py fixed

--- lab_3_keywords_textrank/start.py
+++ lab_3_keywords_textrank/start.py
@@ -48,16 +48,16 @@ if __name__ == "__main__":
     if encoded_tokens:
         print(extract_pairs(encoded_tokens, 5))
 
-    #   mark 6: extract keywords with AdjacencyMatrixGraph using VanillaTextRank
-    # adjacency_graph = AdjacencyMatrixGraph()
-    # if encoded_tokens:
-    #     adjacency_graph.fill_from_tokens(encoded_tokens, 5)
-    # text_rank_adj = VanillaTextRank(adjacency_graph)
-    # text_rank_adj.train()
-    # top_keywords_adj = text_rank_adj.get_top_keywords(10)
-    # print(encoded_top_adj := encoder.decode(top_keywords_adj))
+    # mark 6: extract keywords with AdjacencyMatrixGraph using VanillaTextRank
+    adjacency_graph = AdjacencyMatrixGraph()
+    if encoded_tokens:
+        adjacency_graph.fill_from_tokens(encoded_tokens, 5)
+    text_rank_adj = VanillaTextRank(adjacency_graph)
+    text_rank_adj.train()
+    top_keywords_adj = text_rank_adj.get_top_keywords(10)
+    print(encoded_top_adj := encoder.decode(top_keywords_adj))
 
-    #   mark 8: extract keywords with EdgeListGraph using VanillaTextRank
+    # mark 8: extract keywords with EdgeListGraph using VanillaTextRank
     edge_list = EdgeListGraph()
     if encoded_tokens:
         edge_list.fill_from_tokens(encoded_tokens, 5)
@@ -66,16 +66,16 @@ if __name__ == "__main__":
     top_keywords_edge = text_rank_edge.get_top_keywords(10)
     print(encoded_top_edge := encoder.decode(top_keywords_edge))
 
-    #   mark 8: extract keywords with AdjacencyMatrixGraph using PositionBiasedTextRank
-    # if encoded_tokens:
-    #     adjacency_graph.fill_positions(encoded_tokens)
-    # adjacency_graph.calculate_position_weights()
-    # bias_text_rank_adj = PositionBiasedTextRank(adjacency_graph)
-    # bias_text_rank_adj.train()
-    # top_keywords_adj_bias = bias_text_rank_adj.get_top_keywords(10)
-    # print(encoded_top_adj_pos_bias := encoder.decode(top_keywords_adj_bias))
+    # mark 8: extract keywords with AdjacencyMatrixGraph using PositionBiasedTextRank
+    if encoded_tokens:
+        adjacency_graph.fill_positions(encoded_tokens)
+    adjacency_graph.calculate_position_weights()
+    bias_text_rank_adj = PositionBiasedTextRank(adjacency_graph)
+    bias_text_rank_adj.train()
+    top_keywords_adj_bias = bias_text_rank_adj.get_top_keywords(10)
+    print(encoded_top_adj_pos_bias := encoder.decode(top_keywords_adj_bias))
 
-    #   mark 8: extract keywords with EdgeListGraph using PositionBiasedTextRank
+    # mark 8: extract keywords with EdgeListGraph using PositionBiasedTextRank
     if encoded_tokens:
         edge_list.fill_positions(encoded_tokens)
     edge_list.calculate_position_weights()
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     top_keywords_edge_bias = bias_text_rank_edge.get_top_keywords(10)
     print(encoded_top_edge_pos_bias := encoder.decode(top_keywords_edge_bias))
 
-    #   mark 10: comparing methods in csv file
+    # mark 10: comparing methods in csv file
     benchmark = KeywordExtractionBenchmark(eng_stop_words,
                                            tuple(punctuation),
                                            idf,
