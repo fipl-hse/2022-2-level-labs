806696396bd34910b3d374486d328d4d99a44f1d
chezar0 <egorow4.a@yandex.ru>
2022-11-24 20:44:14 +0300

    without mark10 stuff2

--- lab_3_keywords_textrank/main.py
+++ lab_3_keywords_textrank/main.py
@@ -7,10 +7,7 @@ from pathlib import Path
 
 from typing import Optional, Union
 from itertools import combinations
-from lab_1_keywords_tfidf.main import calculate_frequencies, calculate_tf,\
-    calculate_tfidf
-from lab_2_keywords_cooccurrence.main import extract_phrases, extract_candidate_keyword_phrases,\
-    calculate_frequencies_for_content_words, calculate_word_degrees, calculate_word_scores
+
 
 class TextPreprocessor:
     """
