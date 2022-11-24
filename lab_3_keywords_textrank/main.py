518da24303283409e7042b82a8eb284e2ed41723
YuliaVodopyanova <vodopyanovaye@mail.ru>
2022-11-25 00:40:20 +0300

    changes

--- lab_3_keywords_textrank/main.py
+++ lab_3_keywords_textrank/main.py
@@ -368,11 +368,11 @@ class AdjacencyMatrixGraph:
         Computes position weights for all tokens in text
         """
         sum_positions = {}
-        position_weight = 0
+        position_weight = 0.
         # summa = 0.
         for vertex in self._positions:
             for position in self._positions[vertex]:
-                position_weight += 1. / position
+                position_weight += 1 / position
                 sum_positions[vertex] = position_weight
         for key in sum_positions:
             self._position_weights[key] = sum_positions[key] / sum(sum_positions.values())
