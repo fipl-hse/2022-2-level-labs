02e33ee759d305a9837d8c70db58f7ba3fe5659e
pdshh <pdshamanina@gmail.com>
2022-11-30 00:47:51 +0300

    for 8 fix

--- lab_3_keywords_textrank/main.py
+++ lab_3_keywords_textrank/main.py
@@ -386,7 +386,7 @@ class AdjacencyMatrixGraph:
             tokens : tuple[int, ...]
                 sequence of tokens
         """
-        for index, token in enumerate(tokens, 1):
+        for token, index in enumerate(tokens, 1):
             if token in self._positions:
                 self._positions[token] = [token] + [index]
             else:
@@ -557,7 +557,7 @@ class EdgeListGraph:
             tokens : tuple[int, ...]
                 sequence of tokens
         """
-        for index, token in enumerate(tokens, 1):
+        for token, index in enumerate(tokens, 1):
             if token in self._positions:
                 self._positions[token] = [token] + [index]
             else:
@@ -569,6 +569,7 @@ class EdgeListGraph:
         Computes position weights for all tokens in text
         """
         position_weights = {}
+
         for token in self._positions:
             position_weights[token] = sum(1 / position for position in self._positions[token])
         for token in self._positions:
@@ -828,8 +829,6 @@ class TFIDFAdapter:
             tuple[str, ...]:
                 a requested number tokens with the highest importance scores
         """
-        srtd_tokens = sorted(self._scores.items(), key=lambda elem: (-elem[1], elem[0]))
-        return tuple(elem[0] for elem in srtd_tokens)[:n_keywords]
 
 
 class RAKEAdapter:
