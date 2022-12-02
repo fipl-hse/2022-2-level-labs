1b8e989b89f1f52cfa2d5cc09c4525598e5749ac
taisiyasolareva <taisiya.solareva@gmail.com>
2022-12-02 16:58:28 +0300

    fix start py

--- lab_3_keywords_textrank/start.py
+++ lab_3_keywords_textrank/start.py
@@ -20,16 +20,11 @@ from lab_3_keywords_textrank.main import (
 
 if __name__ == "__main__":
 
-    # finding paths to the necessary utils
     PROJECT_ROOT = Path(__file__).parent
     ASSETS_PATH = PROJECT_ROOT / 'assets'
-
-    # reading the text from which keywords are going to be extracted
     TARGET_TEXT_PATH = ASSETS_PATH / 'article.txt'
     with open(TARGET_TEXT_PATH, 'r', encoding='utf-8') as file:
         text = file.read()
-
-    # reading list of stop words
     STOP_WORDS_PATH = ASSETS_PATH / 'stop_words.txt'
     with open(STOP_WORDS_PATH, 'r', encoding='utf-8') as file:
         stop_words = tuple(file.read().split('\n'))
