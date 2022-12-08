7ca7688a9960eb1d04f323f8bfc2d2847f8c8b3d
Maydaymywaytoheaven <tobakunina@edu.hse.ru>
2022-12-09 02:55:02 +0500

    Type checks my beloathed/4

--- lab_2_keywords_cooccurrence/start.py
+++ lab_2_keywords_cooccurrence/start.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     polish_text = read_target_text(ASSETS_PATH / 'polish.txt')
     stop_words = load_stop_words(ASSETS_PATH / 'stopwords.json')  # Incompatible types in assignment
     # (expression has type "Optional[Mapping[str, Sequence[str]]]", variable has type "List[str]")  [assignment]
-    processed_polish = process_text(polish_text, stop_words)
+    processed_polish = process_text(polish_text, stop_words['pl'])
     if processed_polish:
         print('polish_text', get_top_n(process_text(polish_text), 10, 4), "\n")  # Incompatible types in assignment
         # (expression has type "Optional[Mapping[str, Sequence[str]]]", variable has type "List[str]")  [assignment]
