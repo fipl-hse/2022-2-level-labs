a68da0e30dc68ba466e92a7c46a795d8ad6143cd
Timur20033 <timurorigami@mail.ru>
2022-12-08 12:30:38 +0300

    remove changes main

--- lab_3_keywords_textrank/main.py
+++ lab_3_keywords_textrank/main.py
@@ -4,11 +4,6 @@ Extract keywords based on TextRank algorithm
 """
 from pathlib import Path
 from typing import Optional, Union
-import csv
-from lab_1_keywords_tfidf.main import (calculate_frequencies, calculate_tf, calculate_tfidf)
-from lab_2_keywords_cooccurrence.main import (extract_phrases, extract_candidate_keyword_phrases,
-                                              calculate_frequencies_for_content_words, calculate_word_degrees,
-                                              calculate_word_scores)
 
 import csv
 
