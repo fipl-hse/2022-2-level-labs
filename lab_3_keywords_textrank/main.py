05fe51f362e26795dc7cfaec9475438754588990
Darya Balba <dpbalba@edu.hse.ru>
2022-12-12 14:42:49 +0300

    reverted

--- lab_3_keywords_textrank/main.py
+++ lab_3_keywords_textrank/main.py
@@ -2,13 +2,8 @@
 Lab 3
 Extract keywords based on TextRank algorithm
 """
-import os
-import csv
 from pathlib import Path
 from typing import Optional, Union
-from lab_1_keywords_tfidf.main import calculate_frequencies, calculate_tf, calculate_tfidf
-from lab_2_keywords_cooccurrence.main import extract_phrases, extract_candidate_keyword_phrases, \
-    calculate_frequencies_for_content_words, calculate_word_degrees, calculate_word_scores
 
 import csv
 
